af960eaa1e4b797bf868b350d5c860897801d3c6
Alexander Gallwitz <a.gallwitz@t-online.de>
2025-08-08 01:28:12 +0700

    Removed port check, fixed some widths in the header

--- server.py
+++ server.py
@@ -3,7 +3,6 @@ import http.server
 import ssl
 import socketserver
 import os
-import socket
 
 PORT = 8443
 DIRECTORY = "."
@@ -12,19 +11,9 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
-def find_available_port(start_port):
-    port = start_port
-    while port < start_port + 100:
-        try:
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind(('', port))
-                return port
-        except OSError:
-            port += 1
-    raise OSError("No available ports found")
 
 os.chdir(DIRECTORY)
-PORT = find_available_port(PORT)
+
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
     context.load_cert_chain('cert.pem', 'key.pem')
